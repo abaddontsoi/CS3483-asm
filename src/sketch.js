363991ac548e4379f82e6f5f849d186123760a3d
Abaddon Tsoi <mrabaddontsoi@gmail.com>
2025-11-14 15:21:57 +0800

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,20 @@
 var img;
+var videoFeed;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     img = loadImage('./assets/Amogus.png')
+    pixelDensity(1);
+    videoFeed = createCapture(VIDEO);
+    videoFeed.size(width, height);
+    videoFeed.hide();
 }
 function draw() {
-    image(img, 0, 0, img.width, img.height)
+    image(img, 0, 0, img.width, img.height);
+    push();
+    translate(img.width, 0);
+    scale(-1, 1);
+    image(videoFeed, -img.width, 0, img.width, img.height);
+    pop();
 }
